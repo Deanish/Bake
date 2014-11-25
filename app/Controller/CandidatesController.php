f6554b42d78566308f839541cdb3951a5fbe91b2
Deanish M A <m.a.deanish@gmail.com>
2014-11-25 17:18:40 +0530

    Demo 2

--- app/Controller/CandidatesController.php
+++ app/Controller/CandidatesController.php
@@ -10,6 +10,11 @@ App::uses('AppController', 'Controller');
 			'Search.Prg'
 		);
 
+		// public $uses = array(
+		//         'Interest',
+		//         'Featuredjob',
+		//         'Candidate'
+		// );
 		public $presetVars = array( 
 			array('field' => 'title', 'type' => 'value'),
 			array('field' => 'location', 'type' => 'value'),
@@ -91,9 +96,9 @@ App::uses('AppController', 'Controller');
 			$options = array('conditions' => array('Candidate.' . $this->Candidate->primaryKey => $id));
 			$this->set('user', $this->Candidate->find('first', $options));
 
-			$options = array('conditions' => array('Interest.' . $this->Interest->primaryKey => $id));
+/*			$options = array('conditions' => array('Interest.' . $this->Interest->primaryKey => $id));
 			$this->set('interests', $this->Interest->find('first', $options));
-
+*/
 			if ($this->request->is('post')) {
 				if (AuthComponent::user('role') == 3) {
 					$this->Featuredjob->create();
@@ -106,12 +111,12 @@ App::uses('AppController', 'Controller');
 					}
 				}
 				else {
-					foreach ($interests as $interest) {
+/*					foreach ($interests as $interest) {
 						if (($interest['Interest']['post_id'] == $this->request->data['Interest']['id']) && ($interest['Interest']['user_id'] == AuthComponent::user('id'))) {
 							$this->Session->setFlash(__('Already Applied'));
 							return $this->redirect(array('controller' => 'candidates', 'action' => 'index'));
 						}
-					}
+					}*/
 					$this->Interest->create();
 					$this->request->data['Interest']['user_id'] = AuthComponent::user('id');
 					if ($this->Interest->save($this->request->data)) {
